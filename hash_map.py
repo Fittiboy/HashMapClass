45ad20adb48b66f073d2bed347d2f0728395e9de
Fitti <28876473+Fittiboy@users.noreply.github.com>
2019-11-22 16:30:46 +0100

    Fix infinite loop when looking up key that is not in array

--- hash_map.py
+++ hash_map.py
@@ -10,19 +10,36 @@ class HashMap:
 	def get_index(self, key):
 		index = self.hash(key)
 		jump = 1
+		start_index = 1
+		checked_indices = [index]
 		while self.array[index][0] and self.array[index][0] != key:
+			if index not in checked_indices:
+				checked_indices.append(index)
+			else:
+				index = start_index % self.array_size
+				jump = 1
+				start_index += 1
+			if len(checked_indices) == self.array_size:
+				return None
 			index = (index + jump**2) % self.array_size
 			jump += 1
+			print(checked_indices)
 		return index
 
 	def assign(self, key, value):
 		index = self.get_index(key)
-		self.array[index] = [key, value]
+		if index is not None:
+			self.array[index] = [key, value]
+		else:
+			print("Sorry, the array is full!")
 
 
 	def retrieve(self, key):
 		index = self.get_index(key)
-		return self.array[index][1]
+		if index is not None:
+			return self.array[index][1]
+		else:
+			print("There is no value stored for that key.")
 
 hash_map = HashMap(20)
 
