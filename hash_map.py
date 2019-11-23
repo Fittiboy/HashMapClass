ddc154ac6c269a9bbdae930482a86a20f2adb8e6
Fitti <28876473+Fittiboy@users.noreply.github.com>
2019-11-23 03:32:42 +0100

    Move testing to separate file

--- hash_map.py
+++ hash_map.py
@@ -33,36 +33,3 @@ class HashMap:
 			return self.array[index][1]
 		else:
 			print("There is no value stored for that key.")
-
-
-def test_speed(length):
-	s = t.now()
-	hash_map = HashMap(length)
-
-	for i in range(length):
-		hash_map.assign(str(i), i)
-
-	for i in range(length):
-		hash_map.retrieve(str(i))
-
-	e = t.now()
-	hm_time = e - s
-
-	s = t.now()
-	dictionary = {}
-	for i in range(length):
-		dictionary[str(i)] = 1
-
-	for i in range(length):
-		dictionary[str(i)]
-
-	e = t.now()
-	dict_time = e - s
-
-	hm_string = f"{hm_time.seconds}.{hm_time.microseconds}s"
-	dict_string = f"{dict_time.seconds}.{dict_time.microseconds}s"
-	print(f"Creation and lookup of {length} key-value pairs took {hm_string}",
-		f"for the hash table and {dict_string} for the dictionary.")
-
-for i in range(3):
-	test_speed(10 * 10 ** i)
---
+++ speed_testing.py
@@ -0,0 +1,33 @@
+from hash_map import *
+
+def test_speed(length):
+	s = t.now()
+	hash_map = HashMap(length)
+
+	for i in range(length):
+		hash_map.assign(str(i), i)
+
+	for i in range(length):
+		hash_map.retrieve(str(i))
+
+	e = t.now()
+	hm_time = e - s
+
+	s = t.now()
+	dictionary = {}
+	for i in range(length):
+		dictionary[str(i)] = 1
+
+	for i in range(length):
+		dictionary[str(i)]
+
+	e = t.now()
+	dict_time = e - s
+
+	hm_string = f"{hm_time.seconds}.{hm_time.microseconds}s"
+	dict_string = f"{dict_time.seconds}.{dict_time.microseconds}s"
+	print(f"Creation and lookup of {length} key-value pairs took {hm_string}",
+		f"for the hash table and {dict_string} for the dictionary.")
+
+for i in range(3):
+	test_speed(10 * 10 ** i)
